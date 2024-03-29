c45b36e20113a25a5e62ca2b8e2eb4281deafabd
teddy1565 <teddy1565@gmail.com>
2024-03-30 06:58:43 +0800

    feat: 🎸 first implement

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,19 +1,1 @@
 
-export interface IRemoteSerialportServer {
-    SERVER_PORT: number;
-    SERIALPORT_NAMESPACE_REGEXP: RegExp | string;
-
-    /**
-     * start the server
-     * @returns void
-     */
-    start: () => void;
-
-    /**
-     * register event listener
-     * @param event - event name
-     * @param listener - event listener
-     * @returns
-     */
-    on: (event: string, listener: (...args: any[]) => void) => void;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,78 @@
 
-import { ServerOptions, Server as SocketServer } from "socket.io";
+import { Namespace, ServerOptions, Server as SocketServer, Socket } from "socket.io";
 import { SerialPort } from "serialport";
-import { IRemoteSerialportServer } from "./index.d";
+import { RemoteSerialServerSocketNamespace, RemoteSerialServerSocket } from "./modules/socket";
+import { AbsRemoteSerialServer } from "./types/remote-serialport-types/src/remote-serial-server.model";
+import { DefaultEventsMap } from "socket.io/dist/typed-events";
 
-export class RemoteSerialportServer implements IRemoteSerialportServer {
-    public SERVER_PORT: number;
-    public SERIALPORT_NAMESPACE_REGEXP: RegExp | string;
+function namespace_convert_to_serialport_path(namespace: string): string {
+    if (namespace.match(/^(\/COM)[0-9]+$/) !== null) {
+        return namespace.replace("/", "");
+    }
+    return namespace;
+}
 
-    private serialport_map: Map<string, SerialPort> = new Map();
+export class RemoteSerialportServer extends AbsRemoteSerialServer<RemoteSerialServerSocket, RemoteSerialServerSocketNamespace> {
+    protected SERVER_PORT: number;
 
-    private io: SocketServer;
+    /**
+     * @description
+     * Default RegExp for Serial Port Namespace
+     *
+     * because of socket.io namespace limitation, COM port must have a '/' prefix, but it will replace in serialport path binding
+     * @example
+     * /^(\/dev\/tty(USB|AMA|ACM)|\/COM)[0-9]+$/;
+     */
+    protected SERIALPORT_NAMESPACE_REGEXP: RegExp | string;
 
-    constructor(socket_server_options?: Partial<ServerOptions>, server_port: number = 17991, serialport_namespace_regexp: RegExp | string = /^(\/dev\/tty(USB|AMA|ACM)|COM)[0-9]+$/) {
+    public io: SocketServer;
+
+    protected create_remote_serial_server_socket_namespace(namespace: Namespace<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>): RemoteSerialServerSocketNamespace {
+        return new RemoteSerialServerSocketNamespace(namespace);
+    }
+
+    protected create_remote_serial_server_socket_port(socket: Socket): RemoteSerialServerSocket {
+        return new RemoteSerialServerSocket(socket);
+    }
+
+    constructor(socket_server_options?: Partial<ServerOptions>, server_port: number = 17991, serialport_namespace_regexp: RegExp | string = /^(\/dev\/tty(USB|AMA|ACM)|\/COM)[0-9]+$/) {
+        super();
         this.SERVER_PORT = server_port;
         this.SERIALPORT_NAMESPACE_REGEXP = serialport_namespace_regexp;
         this.io = new SocketServer(socket_server_options);
     }
 
-    start(): void {
-        this.io.listen(this.SERVER_PORT);
-        console.log(`Socket Server Listening on Port ${this.SERVER_PORT}`);
-
-        this.io.of(this.SERIALPORT_NAMESPACE_REGEXP).on("connection", async (socket) => {
-            const select_serialport_path = socket.nsp.name;
-            console.log(`Socket Connected to Server on Port ${this.SERVER_PORT}, of: ${socket.nsp.name}`);
-            socket.on("disconnect", () => {
-                console.log("Socket Disconnected");
-            });
-
-            try {
-                const serialport_list = await SerialPort.list();
-                const selected_serialport = serialport_list.find((serialport) => serialport.path === select_serialport_path);
-                if (!selected_serialport) {
-                    throw new Error(`Serial Port ${select_serialport_path} Not Found`);
-                }
-                const serialport = new SerialPort({
-                    path: selected_serialport.path,
-                    baudRate: 9600
-                });
-                this.serialport_map.set(select_serialport_path, serialport);
-            } catch (error) {
-                console.log(error);
-                return socket.disconnect();
-            }
-        });
-    }
-
-    on(event: string, listener: (...args: any[]) => void): void {
-        this.io.on(event, listener);
-    }
-
-    of(namespace: RegExp | string) {
-        return this.io.of(namespace);
-    }
 }
+
+
+// public start(): void {
+//     this.io.listen(this.SERVER_PORT);
+//     console.log(`Socket Server Listening on Port ${this.SERVER_PORT}`);
+
+//     this.io.of(this.SERIALPORT_NAMESPACE_REGEXP).on("connection", async (socket) => {
+//         const select_serialport_path = namespace_convert_to_serialport_path(socket.nsp.name);
+
+//         console.log(`Socket Connected to Server on Port ${this.SERVER_PORT}, of: ${socket.nsp.name}`);
+
+//         socket.on("disconnect", () => {
+//             console.log("Socket Disconnected");
+//         });
+
+//         try {
+//             const serialport_list = await SerialPort.list();
+//             const selected_serialport = serialport_list.find((serialport) => serialport.path === select_serialport_path);
+//             if (selected_serialport === undefined) {
+//                 socket.emit("")
+//                 throw new Error(`Serial Port ${select_serialport_path} Not Found`);
+//             }
+//             const serialport = new SerialPort({
+//                 path: selected_serialport.path,
+//                 baudRate: 9600
+//             });
+//             this.serialport_map.set(select_serialport_path, serialport);
+//         } catch (error) {
+//             console.log(error);
+//             return socket.disconnect();
+//         }
+//     });
+// }
